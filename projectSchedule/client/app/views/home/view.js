0fd5ad02935dfbf7c43fa7c07d4b391569cdde2f
Delermando <delsantos@hotmail.com.br>
2015-03-17 08:31:45 -0300

    removendo arquivos

--- projectSchedule/client/app/views/home/view.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('api.home', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'views/home/view.html'
-  });
-}]);
