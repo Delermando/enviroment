771544683369c7fc87964ada9b2404e4f84dda5c
Delermando <delsantos@hotmail.com.br>
2015-03-25 08:22:00 -0300

    instalando silex

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo 'Projeto SilexExamplesToArticle';
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/', function () {
+    return 'Hello World!!';
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$name . '!';
+});
+
+
+$app->run();
