1e7ecc7b647ce1cb057c968d0024ce394bb970d3
Delermando <delsantos@hotmail.com.br>
2015-04-03 13:19:30 -0300

    modificando index

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <?php
-echo 'Projeto Enviroment! https://github.com/Delermando/enviroment';
+echo '<h1>Projeto Enviroment!</h1><br /> <a href="https://github.com/Delermando/enviroment">https://github.com/Delermando/enviroment</a>';
