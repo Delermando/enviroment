8924785b0b57bc03574aeeb73274cfa6fdba47bf
Delermando <delsantos@hotmail.com.br>
2015-04-05 15:53:22 -0300

    printando conteudo estatico

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
-var http = require('http');
-var path = require('path');
-var fs = require('fs');
+var http = require( 'http' );
+var path = require( 'path' );
+var fs = require( 'fs' );
 
-http.createServer(function (request, response) {
+var mimeTypes = {
+    '.js' : 'text/javascript',
+    '.html' : 'text/html',
+    '.css' : 'text/css'
+};
+
+http.createServer( function( request, response ) {
     var lookup = path.basename( decodeURI( request.url )) || 'index.html';    
 
     var f = 'content/' + lookup;
-    fs.exists( f, function(exists){
-        console.log(exists ? lookup + " is there" : lookup + " doesn\'t exists ");
+    fs.exists( f, function( exists ){
+        if( exists ){
+            fs.readFile( f, function( err, data ){
+                if( err ){
+                    response.writeHead( 500 );
+                    response.end( 'Server Error!' ); 
+                    return;
+                }
+                var headers = { 'Content-type': mimeTypes[ path.extname( lookup ) ] };
+                response.writeHead( 200, 'headers' );
+                response.end( data );
+            });
+            return; 
+        }
+        response.writeHead( 404 );//no such file found
+        response.end();
     });
 
-}).listen(8080);
+}).listen( 8080 );
