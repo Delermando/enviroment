6734207be07bfd82d7caaf3f5279132eacf6feb5
Delermando <delsantos@hotmail.com.br>
2015-04-05 15:36:30 -0300

    check se arquivo existe

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 var http = require('http');
-var url = require('url');
-
-var pages = [
-    {id: '1', route: '', output: 'WOhooo'},
-    {id: '2', route: '/about', output: 'A simple route with node example'},
-    {id: '3', route: 'anotherPage', output: function () {return 'Here\'s ' + this.route;}},
-];
+var path = require('path');
+var fs = require('fs');
 
 http.createServer(function (request, response) {
-    var id = url.parse(decodeURI(request.url), true).query.id;
+    var lookup = path.basename( decodeURI( request.url )) || 'index.html';    
 
-    if( id ){
-        pages.forEach(function (page) {
-            if (page.id === id) {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.end(typeof page.output === 'function' ? page.output() : page.output);
-            }
-        });
-    }
-    if (!response.finished) {
-        response.writeHead(404);
-        response.end('Page Not Found!');
-    }
+    var f = 'content/' + lookup;
+    fs.exists( f, function(exists){
+        console.log(exists ? lookup + " is there" : lookup + " doesn\'t exists ");
+    });
 
 }).listen(8080);
