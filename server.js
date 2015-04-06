2b4aa1764d7da8e986c37bcd6a4fdeb6feeb7421
Delermando <delsantos@hotmail.com.br>
2015-04-05 21:39:11 -0300

    criando cache no node

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require( 'http' );
-var path = require( 'path' );
+var path = require(
+ 'path' );
 var fs = require( 'fs' );
 
 var mimeTypes = {
@@ -8,7 +9,27 @@ var mimeTypes = {
     '.css' : 'text/css'
 };
 
-http.createServer( function( request, response ) {
+var cache = {};
+function cacheAndDeliver( f, cb )
+{
+    if(!cache[f])
+    {
+        fs.readFile( f, function( err, data ){
+            if( !err )
+            {
+                cache[ f ] = { content: data }
+            }
+            cb( err, data );
+        }); 
+        return;
+    }
+    console.log( 'loading ' + f + ' from cache');
+    cb( null, cache[ f ].content );
+}
+
+
+http.createServer( function( request, response ) 
+{
     var lookup = path.basename( decodeURI( request.url )) || 'index.html';    
 
     var f = 'content/' + lookup;
@@ -16,6 +37,19 @@ http.createServer( function( request, response ) {
     {
         if( exists )
         {
+            cacheAndDeliver(f, function( err, data ){
+                if( err )
+                {
+                    response.writeHead( 500 );
+                    response.end( 'Server Error!' )
+                    return;
+                }
+                var headers = { 'Content-type': mimeTypes[ path.extname( f ) ] };
+                response.writeHead( 200, headers );
+                response.end(data);
+                return;
+            });
+
             fs.readFile( f, function( err, data )
             {
                 if( err )
@@ -38,7 +72,7 @@ http.createServer( function( request, response ) {
 
         }
         response.writeHead( 404 );//no such file found
-        response.end();
+        response.end( 'Page not found!' );
     });
 
 }).listen( 8080 );
