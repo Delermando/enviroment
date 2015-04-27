19723ba33d660358ac00a33573ffaf6120efe244
delermando <delsantos@hotmail.com.br>
2015-04-26 23:09:53 -0300

    buscando um unico item, buscando por querystring

--- app.js
+++ app.js
@@ -14,14 +14,31 @@ var bookRouter = express.Router();
 
 bookRouter.route('/books')
     .get( function( req, res ){
-        Book.find(function( err, books){
+        var query = {};
+        if( req.query.genre ){
+            query.genre = req.query.genre;
+        }
+
+        Book.find( query, function( err, books){
             if( err )
-                console.log( err );
+                res.status(500).send( err );
             else
-                res.json(books);
+                res.json( books );
         });
 
     });
+
+bookRouter.route("/books/:bookId")
+    .get( function( req, res ){
+        res.json(req.params.bookId);
+            Book.findById( req.params.bookId, function( err, books){
+                if( err )
+                    res.status(500).send( err );
+                else
+                    res.json(book);
+            });
+        });
+
 
 app.use('/api', bookRouter );
 
